2e6688570f97f7047743ef887ab575fd6de646e9
runit-godash <runitsharma@godash.ai>
2024-09-17 15:16:03 +0530

    Hosts added

--- bank_query/settings.py
+++ bank_query/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [https://bank-branches-1.onrender.com]
+ALLOWED_HOSTS = ['https://bank-branches-1.onrender.com']
 
 
 # Application definition
